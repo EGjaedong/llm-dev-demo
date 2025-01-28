34b27e65e00b2536e0069c91ef20760200d2547e
zhihenghe <hzh_innovation@163.com>
2025-01-28 09:14:51 +0800

    [zhiheng he] feat: init repo

---
+++ src/main/java/llm/dev/llmdevdemo/LlmDevDemoApplication.java
@@ -0,0 +1,13 @@
+package llm.dev.llmdevdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmDevDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmDevDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/llm/dev/llmdevdemo/LlmDevDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package llm.dev.llmdevdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmDevDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
